dd3c9ae2f7ef92dfbdd177cef1eb01b1a871774c
modusinternet <vince@modusinternet.com>
2020-11-20 01:06:21 -0800

    asdf

--- ccmsusr/index.php
+++ ccmsusr/index.php
@@ -9,15 +9,6 @@
 $CFG = array();
 $CLEAN = array();
 
-
-
-echo "REAL_DOCUMENT_ROOT=[".$_SERVER["REAL_DOCUMENT_ROOT"]."]<br>";
-echo "DOCUMENT_ROOT=[".$_SERVER["DOCUMENT_ROOT"]."]<br>";
-exit;
-
-
-
-
 // Necessary to solve a problem on GoDaddy servers when running sites found in sub folders of existing sites.
 if($_SERVER["REAL_DOCUMENT_ROOT"]) {
 	$_SERVER["DOCUMENT_ROOT"] = $_SERVER["REAL_DOCUMENT_ROOT"];
