9fd0705b3e2e126fb7d827bb82b93f63b68953da
modusinternet <vince@modusinternet.com>
2020-11-30 17:49:37 -0800

    asdf

--- ccmsusr/index.php
+++ ccmsusr/index.php
@@ -39,16 +39,18 @@ CCMS_User_Filter($_SERVER + $_REQUEST, $whitelist);
 CCMS_Set_SESSION();
 
 //echo "1";
-//print_r($_SESSION);
+print_r($_SESSION);
 //session_destroy();
-phpinfo();
-die();
+//phpinfo();
+//die();
 
 if(!isset($_SESSION["USER_ID"]) || $CLEAN["logout"] == "1" || $CLEAN["login"] == "1") {
 	$CLEAN["ccms_tpl"] = "login";
 }
 
 //echo "CLEAN[ccms_tpl]=[".$CLEAN["ccms_tpl"]."]";
+phpinfo();
+die();
 
 CCMS_Main();
 
