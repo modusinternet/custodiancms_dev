f26b9beb5bf6daf364d9632d3d0d997370f441df
modusinternet <vince@modusinternet.com>
2021-01-25 20:09:12 -0800

    asdf

--- ccmstpl/examples/_js/main.js
+++ ccmstpl/examples/_js/main.js
@@ -15,71 +15,47 @@ window.setTimeout(function(){
 var lazyloadImages;
 
 if ("IntersectionObserver" in window) {
-    lazyloadImages = document.querySelectorAll(".lazy");
-    var imageObserver = new IntersectionObserver(function(entries, observer) {
-        entries.forEach(function(entry) {
-            if (entry.isIntersecting) {
-                var image = entry.target;
-                image.src = image.dataset.src;
-                image.classList.remove("lazy");
-                imageObserver.unobserve(image);
-            }
-        });
-    }, {
-        rootMargin: '0px 0px 150px 0px'
-    });
+	lazyloadImages = document.querySelectorAll(".lazy");
+	var imageObserver = new IntersectionObserver(function(entries, observer) {
+		entries.forEach(function(entry) {
+			if (entry.isIntersecting) {
+				var image = entry.target;
+				image.classList.remove("lazy");
+				imageObserver.unobserve(image);
+			}
+		});
+	});
 
-    lazyloadImages.forEach(function(image) {
-        imageObserver.observe(image);
-    });
+	lazyloadImages.forEach(function(image) {
+		imageObserver.observe(image);
+	});
 } else {
-    var lazyloadThrottleTimeout;
-    lazyloadImages = document.var lazyloadImages;
+	var lazyloadThrottleTimeout;
+	lazyloadImages = document.querySelectorAll(".lazy");
 
-  if ("IntersectionObserver" in window) {
-    lazyloadImages = document.querySelectorAll(".lazy");
-    var imageObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          var image = entry.target;
-          image.src = image.dataset.src;
-          image.classList.remove("lazy");
-          imageObserver.unobserve(image);
-        }
-      });
-    });
+	function lazyload () {
+		if(lazyloadThrottleTimeout) {
+			clearTimeout(lazyloadThrottleTimeout);
+		}
 
-    lazyloadImages.forEach(function(image) {
-      imageObserver.observe(image);
-    });
-  } else {
-    var lazyloadThrottleTimeout;
-    lazyloadImages = document.querySelectorAll(".lazy");
+		lazyloadThrottleTimeout = setTimeout(function() {
+			var scrollTop = window.pageYOffset;
+			lazyloadImages.forEach(function(img) {
+				if(img.offsetTop < (window.innerHeight + scrollTop)) {
+					img.src = img.dataset.src;
+					img.classList.remove('lazy');
+				}
+			});
+			if(lazyloadImages.length == 0) {
+				document.removeEventListener("scroll", lazyload);
+				window.removeEventListener("resize", lazyload);
+				window.removeEventListener("orientationChange", lazyload);
+			}
+		}, 20);
+	}
 
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }
-
-      lazyloadThrottleTimeout = setTimeout(function() {
-        var scrollTop = window.pageYOffset;
-        lazyloadImages.forEach(function(img) {
-            if(img.offsetTop < (window.innerHeight + scrollTop)) {
-              img.src = img.dataset.src;
-              img.classList.remove('lazy');
-            }
-        });
-        if(lazyloadImages.length == 0) {
-          document.removeEventListener("scroll", lazyload);
-          window.removeEventListener("resize", lazyload);
-          window.removeEventListener("orientationChange", lazyload);
-        }
-      }, 20);
-    }
-
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
-  }
+	document.addEventListener("scroll", lazyload);
+	window.addEventListener("resize", lazyload);
+	window.addEventListener("orientationChange", lazyload);
 }
 /* Lazyload Background Images End */
