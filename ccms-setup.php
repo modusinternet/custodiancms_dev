bc18656d7c84db21c8adc70847b22de93f089dca
modusinternet <vince@modusinternet.com>
2021-02-18 18:55:35 -0800

    asdf

--- ccms-setup.php
+++ ccms-setup.php
@@ -119,7 +119,7 @@ define('CONFIG_FILE', __FILE__);
 
 // If there's authorization error, set the correct HTTP header.
 if(!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN || SECRET_ACCESS_TOKEN === 'ChangeThisStringToSomethingElseAndUseItInTheURL') {
-	header($_SERVER['SERVER_PROTOCOL'] . ", 403 Forbidden, missing SECRET_ACCESS_TOKEN in URI.", true, 403);
+	header("403 Forbidden, missing SECRET_ACCESS_TOKEN in URI.", true, 403);
 	/*header($_SERVER['SERVER_PROTOCOL'] . " 403 Forbidden", true, 403);*/
 } else {
 	header("Content-Type: text/html; charset=UTF-8");
