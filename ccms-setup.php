d10772578fa7f441cab0feca2a3d6653c36522e2
modusinternet <vince@modusinternet.com>
2021-02-23 22:08:00 -0800

    asdf

--- ccms-setup.php
+++ ccms-setup.php
@@ -366,7 +366,7 @@ if(!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN || SECRET_ACCESS
 
 
 		<div class="tab">
-			<button class="tabLinks" id="defaultOpen" onclick="openTab(event,'tab001')">Welcome</button>
+			<button class="tabLinks active" onclick="openTab(event,'tab001')">Welcome</button>
 			<button class="tabLinks" onclick="openTab(event,'tab002')">Install</button>
 			<button class="tabLinks" onclick="openTab(event,'tab003')">Setup</button>
 			<button class="tabLinks" onclick="openTab(event,'tab004')">Copyright</button>
@@ -452,7 +452,7 @@ if(!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN || SECRET_ACCESS
 			/* Load the English version of the site END */
 
 			/* Show the element with class="defaultOpen" START */
-			document.getElementById("defaultOpen").click();
+			/*document.getElementById("defaultOpen").click();*/
 			/*document.getElementsByClassName("defaultOpen").click();*/
 			/* Show the element with class="defaultOpen" END */
 
