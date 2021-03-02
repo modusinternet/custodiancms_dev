b477f61230bb3ceb420814307559fd58133467bd
modusinternet <vince@modusinternet.com>
2021-03-02 01:35:36 -0800

    asdf

--- ccms-setup.php
+++ ccms-setup.php
@@ -410,7 +410,7 @@ if(!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN || SECRET_ACCESS
 								var respJson = JSON.parse(resp);
 
 
-								for(const [key, value] of resp) {
+								for(const [key, value] of respJson) {
 									console.log("key="+key+"\n");
 									console.log("value="+value+"\n\n");
 								}
